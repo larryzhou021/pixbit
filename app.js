dc0242c5275955e750f95bb4b4bf193ba7ff48d2
Larry.Zhou <larry.zhou@keyfactroy.group>
2019-05-22 16:55:28 +0800

    add node-rpi-rgb-led-matrix

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+var LedMatrix = require("easybotics-rpi-rgb-led-matrix");
+
+//init a 16 rows  by 16 cols led matrix 
+//default hardware mapping is 'regular', could be 'adafruit-hat-pwm' ect 
+var matrix = new LedMatrix(16, 16 );
+matrix.fill(255, 50, 100);
+matrix.setPixel(0, 0, 0, 50, 255);
+matrix.update();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
